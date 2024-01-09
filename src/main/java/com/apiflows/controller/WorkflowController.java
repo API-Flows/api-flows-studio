4f790e9d9948147d1333b3b4e29e971a9c54ba6a
Beppe Catanese <gcatanese@yahoo.com>
2024-01-09 09:49:53 +0100

    Download file endpoint

--- src/main/java/com/apiflows/controller/WorkflowController.java
+++ src/main/java/com/apiflows/controller/WorkflowController.java
@@ -6,12 +6,18 @@ import com.apiflows.service.WorkflowService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  *
@@ -36,6 +42,25 @@ public class WorkflowController {
 
         return new ResponseEntity<>(view, HttpStatus.ACCEPTED);
 
+    }
+
+    @GetMapping("/get/{filePath}")
+    public ResponseEntity<byte[]> getTextFile(@PathVariable String filePath) throws IOException {
+        log.info(filePath);
+        Resource resource = new ClassPathResource(filePath);
+        Path path = Paths.get(resource.getURI());
+
+        byte[] fileContent = Files.readAllBytes(path);
+
+        // Set up HTTP headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        headers.setContentDispositionFormData("attachment", "textfile.txt");
+
+        // Return the file content as a ResponseEntity
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(fileContent);
     }
 
 }
