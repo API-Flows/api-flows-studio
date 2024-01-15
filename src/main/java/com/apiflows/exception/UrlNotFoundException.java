3c1c2b2a385e01e8d3aaa3e089388c0deb9d0279
Beppe Catanese <gcatanese@yahoo.com>
2024-01-15 21:11:34 +0100

    Not found exception

--- src/main/java/com/apiflows/exception/UrlNotFoundException.java
+++ src/main/java/com/apiflows/exception/UrlNotFoundException.java
@@ -1,5 +1,9 @@
 package com.apiflows.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class UrlNotFoundException extends RuntimeException{
 
     public UrlNotFoundException(String message) {
