6e3c0de6206dda6fc7e5506d1c3f30c7de4dcb6c
Beppe Catanese <gcatanese@yahoo.com>
2024-02-03 16:20:17 +0100

    Navigate to component ref

--- react-app/src/home/viewer/InputDetails.js
+++ react-app/src/home/viewer/InputDetails.js
@@ -12,7 +12,7 @@ const InputDetails = ({ inputs, navigateToTab }) => {
     return (
         <>
         {inputs && <ListProperties properties={inputs.properties} navigateToTab={navigateToTab} />}
-        {inputs && <ShowRef $ref={inputs.$ref} />}
+        {inputs && <ShowRef $ref={inputs.$ref} navigateToTab={navigateToTab} />}
         {!inputs && <EmptySection/>}
         </>
     );
@@ -28,7 +28,7 @@ const ListProperties = ({ properties, navigateToTab }) => {
             {Object.entries(properties).map(([key, value]) => (
                 <>
                 <tr>
-                    <td colspan="3" align="left"><Typography>{key}</Typography></td>
+                    <td colspan="3" align="left"><Typography>- {key}</Typography></td>
                 </tr>
                 <tr>
                     <td align="left" valign="top" width="1%"></td>
@@ -52,6 +52,11 @@ const ListProperties = ({ properties, navigateToTab }) => {
                     <td align="right" valign="top" width="10%"><Typography>description:</Typography></td>
                     <td align="left" valign="top" ><Typography>{value.description}</Typography></td>
                 </tr>}
+                {value.properties && <tr>
+                    <td align="left" valign="top" width="1%"></td>
+                    <td align="right" valign="top" width="10%">properties:</td>
+                    <td align="left" valign="top" ><ObjectProperties properties={value.properties} navigateToTab={navigateToTab} /></td>
+                </tr>}
                 <tr>
                     <td colspan="3">&nbsp;</td>
                 </tr>
@@ -64,7 +69,7 @@ const ListProperties = ({ properties, navigateToTab }) => {
     }
 }
 
-const ShowRef = ({ $ref }) => {
+const ShowRef = ({ $ref, navigateToTab }) => {
     if ($ref != null) {
         return (
         <>
@@ -73,7 +78,13 @@ const ShowRef = ({ $ref }) => {
             <table width="100%">
             <tr>
                 <td align="right" width="20%"><Typography>$ref:</Typography></td>
-                <td align="left"><Typography>{$ref}</Typography></td>
+                {$ref && !$ref.startsWith("#/components") && <td align="left">
+                        <Typography>{$ref}</Typography></td>}
+                {$ref && $ref.startsWith("#/components") && <td align="left"><Typography>
+                        <Link onClick={() => navigateToTab(2)} color="default" underline="hover" sx = {{ cursor: 'pointer' }}>
+                            {$ref}
+                        </Link>
+                    </Typography></td>}
             </tr>
             </table>
         </Grid>
@@ -82,16 +93,56 @@ const ShowRef = ({ $ref }) => {
     }
 }
 
+const ObjectProperties = ({ properties, navigateToTab }) => {
+    return (
+        <table width="100%">
+            {Object.entries(properties).map(([key, value]) => (
+                <>
+                <tr>
+                    <td colspan="3" align="left"><Typography>- {key}</Typography></td>
+                </tr>
+                {value.$ref && <tr>
+                    <td align="left" valign="top" width="1%"></td>
+                    <td align="right" valign="top" width="10%"><Typography>$ref:</Typography></td>
+                    <td align="left" valign="top" ><Typography>{value.$ref}</Typography></td>
+                    {value.$ref && value.$ref.startsWith("#/components") && <td align="left" valign="top" ><Typography>
+                        <Link onClick={() => navigateToTab(2)} color="default" underline="hover" sx = {{ cursor: 'pointer' }}>
+                            {value.$ref}
+                        </Link>
+                    </Typography></td>}
+
+                </tr>}
+                {value.type && <tr>
+                    <td align="left" valign="top" width="1%"></td>
+                    <td align="right" valign="top" width="10%"><Typography>type:</Typography></td>
+                    <td align="left" valign="top" ><Typography>{value.type}</Typography></td>
+                </tr>}
+                {value.format && <tr>
+                    <td align="left" valign="top" width="1%"></td>
+                    <td align="right" valign="top" width="10%"><Typography>format:</Typography></td>
+                    <td align="left" valign="top" ><Typography>{value.format}</Typography></td>
+                </tr>}
+                {value.description && <tr>
+                    <td align="left" valign="top" width="1%"></td>
+                    <td align="right" valign="top" width="10%"><Typography>description:</Typography></td>
+                    <td align="left" valign="top" ><Typography>{value.description}</Typography></td>
+                </tr>}
+                </>
+            ))}
+        </table>
+    );
+}
+
 const EmptySection = ({ }) => {
-        return (
-        <>
-            <Divider/>
-            <br/>
-            <Box display="flex" justifyContent="center">
-                <Typography>No Inputs is defined</Typography>
-            </Box>
-        </>
-        );
+    return (
+    <>
+        <Divider/>
+        <br/>
+        <Box display="flex" justifyContent="center">
+            <Typography>No Inputs is defined</Typography>
+        </Box>
+    </>
+    );
 }
 
 export default InputDetails;
